2169c2d66d23a86dac6c3723202a742c124d4024
Ram Basnet <Lia03gomes@gmail.com>
2023-09-07 13:33:40 -0600

    Final Submission

---
+++ math/main.py
@@ -0,0 +1,30 @@
+"""
+Math and Variables Lab
+By: FIXME
+CSCI 110 Lab
+Date: FIXME
+ 
+Read and solve: Add Two Numbers - https://open.kattis.com/problems/addtwonumbers 
+ 
+Algorithm steps:
+  1. Read data as a line
+  2. Split the line into two integers
+  3. Add them up
+  4. print the result
+"""
+def main():
+    #read data
+    line = input()
+    #split the data into two numbers 
+    a, b = line.split()
+    #check to see if split correctly 
+    #print (f'{a=},{b=}') #FIXME 1: comment/remove this line before submitting to kattis 
+    #FIXME 2: convert string a into integer 
+    a = int(a)
+    b = int(b)
+    #FIXME 3: convert string b into integer 
+    answer=int(a+b)
+    print(answer)
+    #FIXME 4: add two numbers and store the result into ans variable
+    #FIXME 5: print the answer as shown as sample ouput 
+main()#call main function
