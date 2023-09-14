7ace29c8b5c2d2b394d3b50edddcbe248d9be1a3
Chica Gomes <142940971+Dj-messup@users.noreply.github.com>
2023-09-14 13:39:02 -0600

    Update main.py

--- math/main.py
+++ math/main.py
@@ -19,12 +19,12 @@ def main():
     a, b = line.split()
     #check to see if split correctly 
     #print (f'{a=},{b=}') #FIXME 1: comment/remove this line before submitting to kattis 
-    #FIXME 2: convert string a into integer 
+    #FIXED convert string a into integer 
     a = int(a)
     b = int(b)
-    #FIXME 3: convert string b into integer 
+    #FIXED: convert string b into integer 
     ans=int(a+b)
     print(ans)
-    #FIXME 4: add two numbers and store the result into ans variable
-    #FIXME 5: print the answer as shown as sample ouput 
+    #FIXED: add two numbers and store the result into ans variable
+    #FIXED: print the answer as shown as sample ouput 
 main()#call main function
