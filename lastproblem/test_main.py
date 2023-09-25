5236f0009fbd6bd34219cf7df05e911ab280ba31
Chica Gomes <142940971+Dj-messup@users.noreply.github.com>
2023-09-25 12:34:43 -0600

    Update test_main.py

--- lastproblem/test_main.py
+++ lastproblem/test_main.py
@@ -12,26 +12,26 @@ def test_answer():
     """
     ans = Main.answer("Alice")
     expected = "Thank you, Alice, and farewell!"
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
+    assert ans == expected
     ans = Main.answer("Bob")
-    expected = "Thank you, Bob and farewell!"  # FIXME 5: fix the expected output
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
+    expected = "Thank you, Bob and farewell!"  #fix the expected output
+    assert ans == expected
     # add a new test case to test your answer function
     ans = Main.answer("Twilight")
-    expected = "Thank you, Twilight and farewell!"  # FIXME 5: fix the expected output
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
-    # FIXME 7: add a new test case to test your answer function
+    expected = "Thank you, Twilight and farewell!"  #fix the expected output
+    assert ans == expected
+    # add a new test case to test your answer function
     ans = Main.answer("Apple Jack")
-    expected = "Thank you, Apple Jack and farewell!"  # FIXME 5: fix the expected output
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
-    # FIXME 8: add a new test case to test your answer function
+    expected = "Thank you, Apple Jack and farewell!"  #fix the expected output
+    assert ans == expected
+    # add a new test case to test your answer function
     ans = Main.answer("Fluttershy")
-    expected = "Thank you, Fluttershy and farewell!"  # FIXME 5: fix the expected output
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
-    # FIXME 9: add a new test case to test your answer function
+    expected = "Thank you, Fluttershy and farewell!"  #fix the expected output
+    assert ans == expected
+    # add a new test case to test your answer function
     ans = Main.answer("Rainbow Dash")
-    expected = "Thank you, Rainbow Dash and farewell!"  # FIXME 5: fix the expected output
-    assert ans == expected, f"Expected: {expected}, but got: {ans}"
+    expected = "Thank you, Rainbow Dash and farewell!"  # fix the expected output
+    assert ans == expected
     print("All test cases passed...", file=sys.stderr)
 
 
