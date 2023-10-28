e8390a2af140d405d4d1aae0c90e691705761357
Ram Basnet <Lia03gomes@gmail.com>
2023-10-28 16:45:12 -0600

    Final

--- lists/pet.py
+++ lists/pet.py
@@ -30,48 +30,21 @@ def main() -> None:
         total_score = list_sum(contestant_scores)
         scores.append(total_score)
 
-        # Step 3: Find the sum of scores using list_sum function
-        line_sum = list_sum(input_data)
-        
-        # Step 4: Append the sum to the list of scores
-        scores.append(line_sum)
-    
-    # Step 5: Calculate the winner
-    winner = answer(scores)
-    
-    # Step 6: Print the winner
-    print(f"The winner is contestant {winner[0]} with a total score of {winner[1]} points.")
+    result = answer(scores)
+    print(result)
 
 def get_data() -> List[int]:
-    """Reads the data from std input and returns it as a list of ints
-    Returns:
-        List[int]: list of ints
-    """
-    line = input("Enter 4 grades for a contestant: ")
-    contestant_grades = [int(grade) for grade in line.split()]
-    return contestant_grades
+    str_nums = input().split()
+    return [int(num) for num in str_nums]
 
 def list_sum(numbers: List[int]) -> int:
-    """Finds and returns the sum of the numbers in the list.
-    Args:
-        numbers: List[int]: takes a list of numbers as a parameter
+    return sum(numbers)
 
-    Returns:
-        int: sum of the numbers in the list
-    """
-    return sum(numbers)  # Find the sum of the numbers in the list and return it
-
-def answer(scores: List[int]) -> List[int]:
-    """Calculates the winner of the show based on the highest score.
-    Args:
-        scores (List[int]): List of 5 contestants' scores
-
-    Returns:
-        List[int]: winner's number and points
-    """
+def answer(scores: List[int]) -> str:
     max_score = max(scores)
-    winner_number = scores.index(max_score) + 1
-    return [winner_number, max_score]  # Return the winner's number and points as a list
+    index = scores.index(max_score)
+    # Return the index+1 and the max number in the list as a string
+    return f"{index + 1} {max_score}"
 
 if __name__ == "__main__":
     main()
